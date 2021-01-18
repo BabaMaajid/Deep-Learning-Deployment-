dfe432a902f3a8ca1311bfc41a5be8ef60c1bbe7
Maajid Bashir <maajid.net@gmail.com>
2021-01-18 23:01:40 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -40,12 +40,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('/Users/sudhanshukumar/Downloads/PetImages-20190330T113641Z-001/PetImages',
+training_set = train_datagen.flow_from_directory('/Users/maajids/Downloads/PetImages-20190330T113641Z-001/PetImages',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('/Users/sudhanshukumar/Downloads/PetImages-20190330T113641Z-001/PetImages',
+test_set = test_datagen.flow_from_directory('/Users/maajids/Downloads/PetImages-20190330T113641Z-001/PetImages',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
